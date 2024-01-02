4106de235a223f885f1cf6e468643dba0f04b8f6
jeremyabeard5 <jeremyab5@gmail.com>
2024-01-01 20:11:25 -0700

    finished ch 9 getting data

--- egrep.py
+++ egrep.py
@@ -4,11 +4,12 @@ import sys, re
 # sys.argv is list of command-line arguments
 # [0] is name of program itself
 # [1] is regex specified at command-line
+#print(f"egrep.py: sys.argv[0] is {sys.argv[0]}, sys.argv[1] is {sys.argv[1]}")
 regex = sys.argv[1]
 
 # for every line passed into the script
+#print(f"egrep.py: sys.stdin is: {sys.stdin}")
 for line in sys.stdin:
     # if it matches regex, write it to stdout
     if re.search(regex, line):
         sys.stdout.write(line)
-        
--- line_count.py
+++ line_count.py
@@ -2,7 +2,9 @@
 import sys
 
 count = 0
+#print(f"line_count.py: sys.stdin is {sys.stdin}")
 for line in sys.stdin:
+    print(f"line_count.py: line in sys.stdin is {line}")
     count += 1
     
 # print goes to sys.stdout
